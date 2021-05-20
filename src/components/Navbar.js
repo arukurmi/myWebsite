fb6f83bec093e3490642c0155b6d474e3caea901
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-05-20 19:12:42 +0530

    changing bootstrap from 5.0.0 to 4.5.3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React from "react";
 import logo from "../logo.png";
 
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src={logo} alt="AK logo..."  height="40" width="105" /></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                    <a className="navbar-brand" href="#"><img src={logo} alt="AryanshKurmi logo..." /></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                    </ul>
-
-                </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About ME</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">resume</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">contact me</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </nav>
 
