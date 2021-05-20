7a467e13dcce3483e0427f9b67e631eb51f05869
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-05-20 17:16:00 +0530

    add logo add navbar specifications

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h3>aryansh</h3>
+
+    <>
+      
+      
+      
+      
+      <Navbar />
+      <h3>aryansh</h3>
+
+
+
+
+
+    </>
+
+
   );
 }
 
